f1af41ab9806af7c33979514285795944cd35443
AlexFierro9 <saksham0910@outlook.com>
2023-08-30 23:06:03 +0530

    Basic Functionality and APIs now implemented

---
+++ script.js
@@ -0,0 +1,106 @@
+const chatBox = document.getElementById("chat-box");
+const userInput = document.getElementById("user-input");
+const sendBtn = document.getElementById("send-btn");
+
+function addMessageToChat(message, isUser = false) {
+    const messageClass = isUser ? "user-message" : "bot-message";
+    const messageDiv = document.createElement("div");
+    messageDiv.classList.add("chat-message", messageClass);
+    messageDiv.innerText = message;
+    chatBox.appendChild(messageDiv);
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+
+async function handleUserInput() {
+    const userMessage = userInput.value;
+    userInput.value = "";
+    addMessageToChat(userMessage, true);
+
+    // Call the Flask API for generating response
+    const response = await fetch("http://127.0.0.1:5000/get_response", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ dialog: [userMessage] }),
+    });
+
+    if (response.ok) {
+        const responseData = await response.json();
+        const botResponse = responseData.generated_response;
+        const emotion = responseData.emotion;
+        const apiKey = responseData.api_key;
+
+        addMessageToChat(botResponse);
+
+        // Fetch music recommendations based on emotion
+        const recommendationsUrl = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${emotion}&api_key=${apiKey}&format=json&limit=4`;
+        const recommendationsResponse = await fetch(recommendationsUrl);
+
+        if (recommendationsResponse.ok) {
+            const recommendationsData = await recommendationsResponse.json();
+            const recommendations = recommendationsData.tracks.track;
+            displayRecommendations(recommendations);
+        }
+    }
+}
+
+// ... Rest of the code ...
+
+// ... Existing code ...
+
+function displayRecommendations(recommendations) {
+    const recommendationsContainer = document.createElement("div");
+    recommendationsContainer.classList.add("recommendations-container");
+
+    recommendations.forEach((rec, index) => {
+        const recommendationDiv = document.createElement("div");
+        recommendationDiv.classList.add("recommendation");
+        recommendationDiv.innerHTML = `
+            <p class="track-name">${rec.name}</p>
+            <p class="artist-name">by ${rec.artist.name}</p>
+            <a class="listen-link" href="${rec.url}" target="_blank">Listen</a>
+        `;
+        recommendationsContainer.appendChild(recommendationDiv);
+    });
+
+    // Clear previous recommendations and append the new ones
+    document.querySelector(".recommendations-container").innerHTML = "";
+    document.querySelector(".input-container").appendChild(recommendationsContainer);
+}
+// ... Existing code ...
+
+function displayRecommendations(recommendations) {
+    const recommendationsContainer = document.createElement("div");
+    recommendationsContainer.classList.add("recommendations-container");
+
+    recommendations.forEach((rec, index) => {
+        const recommendationDiv = document.createElement("div");
+        recommendationDiv.classList.add("recommendation");
+        recommendationDiv.innerHTML = `
+            <p class="track-name">${rec.name}</p>
+            <p class="artist-name">by ${rec.artist.name}</p>
+            <a class="listen-link" href="${rec.url}" target="_blank">Listen</a>
+        `;
+        recommendationsContainer.appendChild(recommendationDiv);
+    });
+
+    // Clear previous recommendations and append the new ones
+    const existingRecommendationsContainer = document.querySelector(".recommendations-container");
+    if (existingRecommendationsContainer) {
+        existingRecommendationsContainer.remove();
+    }
+    document.querySelector(".chat-container").appendChild(recommendationsContainer);
+}
+
+// ... Rest of the code ...
+
+
+// ... Rest of the code ...
+
+
+
+
+
+sendBtn.addEventListener("click", handleUserInput);
---
+++ server.py
@@ -0,0 +1,51 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from flask_cors import CORS
+
+sentiment_tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-base-finetuned-emotion")
+sentiment_model = AutoModelForSeq2SeqLM.from_pretrained("mrm8488/t5-base-finetuned-emotion")
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/GODEL-v1_1-large-seq2seq")
+model = AutoModelForSeq2SeqLM.from_pretrained("microsoft/GODEL-v1_1-large-seq2seq")
+
+
+def generate_response(dialog):
+    knowledge = ''
+    instruction = f'Instruction: given a dialog context, you need to response empathically.'
+    dialog_text = ' EOS '.join(dialog)
+    query = f"{instruction} [CONTEXT] {dialog_text} {knowledge}"
+
+    input_ids = tokenizer.encode(query, return_tensors="pt")
+    output = model.generate(input_ids, max_length=16, min_length=2, top_p=0.9, do_sample=True)
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    return generated_text
+
+
+def sentiment_finder(user_dialog):
+    input_ids = sentiment_tokenizer.encode(user_dialog + '</s>', return_tensors='pt')
+    output = sentiment_model.generate(input_ids=input_ids, max_length=2)
+    emotion = [sentiment_tokenizer.decode(ids) for ids in output][0]
+    return emotion
+
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+
+class ResponseResource(Resource):
+    def post(self):
+        data = request.get_json()
+        dialog = data.get('dialog', [])
+        generated_text = generate_response(dialog)
+        user_dialog = dialog[-1]
+        emotion = sentiment_finder(user_dialog)
+        response_data = {'generated_response': generated_text, 'emotion': emotion}
+        return jsonify(response_data)
+
+
+api.add_resource(ResponseResource, '/get_response')
+
+if __name__ == '__main__':
+    app.run()
